f17251ca7338d748c191d9448068e800b699189a
nusratzoda <nusratzodasuhaib@gmail.com>
2023-08-07 17:00:23 -1200

    permission seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -7,6 +7,8 @@ use App\Models\Role;
 use App\Models\Users;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Foundation\Auth\User;
+use Illuminate\Support\Facades\Password;
 
 class UserSeeder extends Seeder
 {
@@ -15,23 +17,32 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        $developer = Role::where('slug','web-developer')->first();
-        $manager = Role::where('slug', 'project-manager')->first();
-        $createTasks = Permission::where('slug','create-tasks')->first();
-        $manageUsers = Permission::where('slug','manage-users')->first();
-        $user1 = new Users();
-        $user1->name = 'Jhon Deo';
-        $user1->email = 'jhon@deo.com';
-        $user1->password = bcrypt('secret');
-        $user1->save();
-        $user1->roles()->attach($developer);
-        $user1->permissions()->attach($createTasks);
-        $user2 = new Users();
-        $user2->name = 'Mike Thomas';
-        $user2->email = 'mike@thomas.com';
-        $user2->password = bcrypt('secret');
-        $user2->save();
-        $user2->roles()->attach($manager);
-        $user2->permissions()->attach($manageUsers);
+        // $developer = Role::where('slug','web-developer')->first();
+        // $manager = Role::where('slug', 'project-manager')->first();
+        // $createTasks = Permission::where('slug','create-tasks')->first();
+        // $manageUsers = Permission::where('slug','manage-users')->first();
+        // $user1 = new Users();
+        // $user1->name = 'Jhon Deo';
+        // $user1->email = 'jhon@deo.com';
+        // $user1->password = bcrypt('secret');
+        // $user1->save();
+        // $user1->roles()->attach($developer);
+        // $user1->permissions()->attach($createTasks);
+        // $user2 = new Users();
+        // $user2->name = 'Mike Thomas';
+        // $user2->email = 'mike@thomas.com';
+        // $user2->password = bcrypt('secret');
+        // $user2->save();
+        // $user2->roles()->attach($manager);
+        // $user2->permissions()->attach($manageUsers);
+
+        $user = User::create(
+            [
+                'name' => 'Admin',
+                'email' => 'admin@lms.com',
+                'password' => Password::hash('password')
+            ]
+            );
+            $user->assignRole('admin');
     }
 }
