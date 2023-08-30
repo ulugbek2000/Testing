e6157509003be76fb1ed969fb76e88435b3fac0a
nusratzoda <nusratzodasuhaib@gmail.com>
2023-08-29 18:09:03 -1200

    Register

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -42,6 +42,7 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\LocaleMiddleware::class,
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
         ],
 
         'api' => [
