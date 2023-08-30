ccac4da63ebca867a5a45b72d11650cfb45266b2
nusratzoda <nusratzodasuhaib@gmail.com>
2023-08-29 14:54:18 -1200

    Register

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -51,7 +51,7 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Session\Middleware\StartSession::class,
             // \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            // \Illuminate\View\Middleware\ShareErrorsFromSession::class,
           
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
