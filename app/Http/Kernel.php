cf456cb29fbbf864bcf681118f3520d5a9ede480
nusratzoda <nusratzodasuhaib@gmail.com>
2023-08-29 14:50:58 -1200

    Register

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -42,7 +42,6 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\LocaleMiddleware::class,
-            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
         ],
 
         'api' => [
@@ -52,6 +51,7 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Session\Middleware\StartSession::class,
             // \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
           
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
