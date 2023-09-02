ec3c06de97b216c3151f7c0e2cb2015eea03ce55
nusratzoda <nusratzodasuhaib@gmail.com>
2023-09-01 18:41:01 -1200

    login

--- app/Http/Middleware/SendTokenToFrontend.php
+++ app/Http/Middleware/SendTokenToFrontend.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\User;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -16,11 +17,22 @@ class SendTokenToFrontend
      */
     public function handle(Request $request, Closure $next): Response
     {
+        // Проверьте наличие заголовка с токеном (замените 'X-Auth-Token' на ваш заголовок)
+        $token = $request->header('Bearer');
 
-        if (Auth::guard('sanctum')->check()) {
-            return $next($request);
+        if (!$token) {
+            return response()->json(['message' => 'Unauthorized'], 401);
         }
 
-        return response()->json(['message' => 'Unauthorized'], 401);
+        $user = User::where('api_token', $token)->first();
+
+        if (!$user) {
+            return response()->json(['message' => 'Unauthorized'], 401);
+        }
+
+        // Аутентификация успешна, установите пользователя как аутентифицированного
+        Auth::login($user);
+
+        return $next($request);
     }
 }
