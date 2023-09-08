0361c3ef782b2c974e188cc325cf4c143d9fd833
nusratzoda <nusratzodasuhaib@gmail.com>
2023-09-07 15:43:02 -1200

    id

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
-use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
 
 class ProfileController extends Controller
@@ -13,7 +15,7 @@ class ProfileController extends Controller
     public function updateProfile(Request $request)
     {
         $user = Auth::user();
-        $request->validate([
+        $validator = Validator::make(request()->all(), [
             'name' => 'string',
             'surname' => 'string',
             'email' => 'email|unique:users,email,' . $user->id,
@@ -24,27 +26,41 @@ class ProfileController extends Controller
             'gender' => 'string|in:male,female,other',
             'date_of_birth' => 'date',
         ]);
-
-        $photoPath = $user->photo;
-
-
-        if ($request->hasFile('photo')) {
-            // Delete old cover file if needed
-            Storage::delete($user->photo);
-            // Upload and store new cover file
-            $photoPath = $request->file('photo')->store('photo', 'public');
+        
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
         }
-        $data = array_merge($request->only(['name', 'surname', 'email', 'phone', 'city', 'gender', 'date_of_birth']), [
-            'photo' => $photoPath
-        ]);
-
-        $user->update($data);
-
+        
+        // Установите значения атрибутов пользователя на основе запроса
+        $user->name = $request->input('name');
+        $user->surname = $request->input('surname'); // Исправлено на 'surname'
+        $user->email = $request->input('email'); // Исправлено на 'email'
+        $user->phone = $request->input('phone');
+        $user->city = $request->input('city');
+        $user->gender = $request->input('gender');
+        $user->date_of_birth = $request->input('date_of_birth');
+        
         if ($request->has('password')) {
             $user->password = bcrypt($request->input('password'));
-            $user->save();
         }
+        
+      
 
+        // if ($request->hasFile('photo')) {
+        //     $destination = 'storage/profile/' . $user->photo;
+        //     if (File::exists($destination)) {
+        //         File::delete($destination);
+        //     }
+        //     $file = $request->file('photo');
+        //     $extension = $file->getClientOriginalExtension();
+        //     $filename = time() . '.' . $extension;
+        //     $file->move('storage/profile/', $filename);
+        //     $user->photo = $filename;
+        // }
+        if ($request->has('password')) {
+            $user->password = bcrypt($request->input('password'));
+        }
+        $user->save();
         return response()->json(['message' => 'Profile updated successfully']);
     }
 }
