9d0f5d10da387410146b1bc7a358d74deed4b33e
nusratzoda <nusratzodasuhaib@gmail.com>
2023-09-07 18:41:41 -1200

    id

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -17,7 +17,7 @@ class ProfileController extends Controller
     public function updateProfile(Request $request)
     {
         $user = Auth::user();
-    
+
         // Проверяем данные из тела запроса
         $validator = Validator::make($request->all(), [
             'name' => 'string',
@@ -34,92 +34,100 @@ class ProfileController extends Controller
         if ($validator->fails()) {
             return response()->json(['errors' => $validator->errors()], 422);
         }
-    
+
         // Обновляем данные пользователя на основе JSON-данных
         $data = [
             'name' => $request->input('name', $user->name),
             'surname' => $request->input('surname', $user->surname),
             'email' => $request->input('email', $user->email),
             'phone' => $request->input('phone', $user->phone),
-            'password' => $request->has('password') ? bcrypt($request->input('password')) : $user->password,
+            // 'password' => $request->has('password') ? bcrypt($request->input('password')) : $user->password,
             'city' => $request->input('city', $user->city),
             'gender' => $request->input('gender', $user->gender),
             'date_of_birth' => $request->input('date_of_birth', $user->date_of_birth),
         ];
-    
+
         // Добавляем обработку изображения, если оно было изменено
-      
-            if (is_string($photoPath) && Storage::exists($photoPath)) {
-                Storage::delete($photoPath);
-            } {
-    
-            $photoPath = $request->file('photo')->store('photo', 'public');
-            $data['photo'] = $photoPath;
+
+        if (is_string($photoPath) && Storage::exists($photoPath)) {
+            Storage::delete($photoPath);
+        }
+        $photoPath = $request->file('photo')->store('photo', 'public');
+        $data['photo'] = $photoPath;
         $user->update($data);
-    
+
+        
+        if ($request->has('password')) {
+            $user->password = bcrypt($request->input('password'));
+            $user->save();
+        }
+
+     
+
+
         return response()->json(['message' => 'Profile updated successfully']);
+
+
+
+
+
+
+
+
+
+
+
+        // public function updateProfile(Request $request)
+        // {
+        //     // dd($request->all());
+        //     $user = Auth::user();
+
+        //     $request->validate([
+        //         'name' => 'string',
+        //         'surname' => 'string',
+        //         'email' => 'email|unique:users,email,' . $user->id,
+        //         'phone' => 'string|unique:users,phone,' . $user->id,
+        //         'password' => 'string|min:8',
+        //         'city' => 'string',
+        //         'photo' => 'nullable|image|mimes:jpeg,png,jpg,gif,mov',
+        //         'gender' => 'string|in:male,female,other',
+        //         'date_of_birth' => 'date',
+        //     ]);
+
+        //     $user = Auth::user();
+
+        //     $request->validate([
+        //         'name' => 'string',
+        //         'surname' => 'string',
+        //         'email' => 'email|unique:users,email,' . $user->id,
+        //         'phone' => 'string|unique:users,phone,' . $user->id,
+        //         'password' => 'string|min:8',
+        //         'city' => 'string',
+        //         'photo' => 'nullable|mimes:jpeg,png,jpg,gif,mov',
+        //         'gender' => 'string|in:male,female,other',
+        //         'date_of_birth' => 'date',
+        //     ]);
+        //     $coverpath = $user->photo;
+        //     if ($request->hasFile('photo')) {
+        //         // Delete old logo file if needed
+        //         Storage::delete($user->photo);
+        //         // Upload and store new logo file
+        //         $coverpath = $request->file('photo')->store('photo', 'public');
+        //     } else {
+        //         $coverpath = $user->photo;
+        //     }
+
+        //     $data = array_merge($request->only(['name', 'surname', 'email', 'phone', 'city', 'gender', 'date_of_birth']), [
+        //         'photo' => $coverpath,
+
+        //     ]);
+        //     $user->update($data);
+        //     if ($request->has('password')) {
+        //         $user->password = bcrypt($request->input('password'));
+        //         $user->save();
+        //     }
+
+        //     return response()->json(['message' => 'Profile updated successfully']);
+        // }
     }
-
-
-
-
-
-
-
-
-
-
-    // public function updateProfile(Request $request)
-    // {
-    //     // dd($request->all());
-    //     $user = Auth::user();
-
-    //     $request->validate([
-    //         'name' => 'string',
-    //         'surname' => 'string',
-    //         'email' => 'email|unique:users,email,' . $user->id,
-    //         'phone' => 'string|unique:users,phone,' . $user->id,
-    //         'password' => 'string|min:8',
-    //         'city' => 'string',
-    //         'photo' => 'nullable|image|mimes:jpeg,png,jpg,gif,mov',
-    //         'gender' => 'string|in:male,female,other',
-    //         'date_of_birth' => 'date',
-    //     ]);
-
-    //     $user = Auth::user();
-
-    //     $request->validate([
-    //         'name' => 'string',
-    //         'surname' => 'string',
-    //         'email' => 'email|unique:users,email,' . $user->id,
-    //         'phone' => 'string|unique:users,phone,' . $user->id,
-    //         'password' => 'string|min:8',
-    //         'city' => 'string',
-    //         'photo' => 'nullable|mimes:jpeg,png,jpg,gif,mov',
-    //         'gender' => 'string|in:male,female,other',
-    //         'date_of_birth' => 'date',
-    //     ]);
-    //     $coverpath = $user->photo;
-    //     if ($request->hasFile('photo')) {
-    //         // Delete old logo file if needed
-    //         Storage::delete($user->photo);
-    //         // Upload and store new logo file
-    //         $coverpath = $request->file('photo')->store('photo', 'public');
-    //     } else {
-    //         $coverpath = $user->photo;
-    //     }
-
-    //     $data = array_merge($request->only(['name', 'surname', 'email', 'phone', 'city', 'gender', 'date_of_birth']), [
-    //         'photo' => $coverpath,
-
-    //     ]);
-    //     $user->update($data);
-    //     if ($request->has('password')) {
-    //         $user->password = bcrypt($request->input('password'));
-    //         $user->save();
-    //     }
-
-    //     return response()->json(['message' => 'Profile updated successfully']);
-    // }
-}
 }
