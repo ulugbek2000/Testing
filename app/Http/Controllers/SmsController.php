2915336674b7bf1c5ca340694907ef78bb4394db
nusratzoda <nusratzodasuhaib@gmail.com>
2023-09-13 12:00:27 -1200

    update

--- app/Http/Controllers/SmsController.php
+++ app/Http/Controllers/SmsController.php
@@ -2,20 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Notifications\SmsVerification;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use OsonSMS\SMSGateway\SMSGateway;
 
 class SmsController extends Controller
 {
     public function sendSms()
     {
-        $txn_id = uniqid();
-        $result = SMSGateway::Send('931272616', 'This is my test message from Laravel!', $txn_id);
+        Auth::user()->notify(new SmsVerification(['text' => 'Raqami yagonai shumo 9854', 'v_no' => 9854, 'number' => '931272616']));
+        // $txn_id = uniqid();
+        // $result = SMSGateway::Send('931272616', 'This is my test message from Laravel!', $txn_id);
 
-        if ($result) {
-            return "SMS has been sent successfully";
-        } else {
-            return "An error occurred while sending the SMS";
-        }
+        // if ($result) {
+        //     return "SMS has been sent successfully";
+        // } else {
+        //     return "An error occurred while sending the SMS";
+        // }
     }
 }
---
+++ app/Notifications/SmsVerification.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use OsonSMS\SMSGateway\SMSGateway;
+
+class SmsVerification extends Notification
+{
+    use Queueable;
+
+    private $message;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct($message)
+    {
+        $this->message = $message;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['database', 'sms'];
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            $this->message
+        ];
+    }
+
+    public function toSms(object $notifiable) {
+        $txn_id = uniqid();
+        $result = SMSGateway::Send($this->message['number'], $this->message['text'], $txn_id);
+
+        if ($result) 
+            return "SMS has been sent successfully";
+        return "An error occurred while sending the SMS";
+    }
+}
