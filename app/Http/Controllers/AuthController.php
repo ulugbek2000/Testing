fbb7c8dc6000c825d62b918902d9bff333207c1f
nusratzoda <nusratzodasuhaib@gmail.com>
2023-09-18 16:37:07 -1200

    CheckStatus

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -69,7 +69,6 @@ class AuthController extends Controller
     function verifyPhoneNumber(Request $request)
     {
         $user = Auth::guard('web')->user();
-dd($user);
         if ($user) {
             // Пользователь аутентифицирован, можно вызывать метод verifyCode()
             if ($user->verifyCode($request->input('verification'))) {
