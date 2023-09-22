55e2e981c5554fc77e78367a8e222fcc06628e41
nusratzoda <nusratzodasuhaib@gmail.com>
2023-09-22 08:59:42 -1200

    Balance

--- app/Http/Controllers/BalanceController.php
+++ app/Http/Controllers/BalanceController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Balance;
 use App\Models\Course;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -24,23 +25,33 @@ class BalanceController extends Controller
         return response()->json('success', 'Balance updated successfully');
     }
 
-    public function purchaseCourse(Request $request, Course $course,User $user)
+    public function purchaseCourse(Request $request, Course $course)
     {
-        // $user = Auth::user();
+        $user = Auth::user();
+        if (!$user->balance) {
+            // Если объект баланса отсутствует, создайте его.
+            $balance = new Balance();
+            $balance->amount = 0; // Устанавливаем начальный баланс в 0 или другое значение.
+            $balance->save();
 
+            // Привяжите объект баланса к пользователю.
+            $user->balance()->associate($balance);
+            $user->save();
+        }
         if (!$course) {
             return response()->json('error', 'Course not found');
         }
 
+        // Проверьте, достаточно ли средств на балансе пользователя.
         if ($user->balance->amount < $course->subscription->price) {
             return response()->json('error', 'Insufficient balance');
         }
 
-        // Уменьшите баланс пользователя
+        // Уменьшите баланс пользователя.
         $user->balance->amount -= $course->subscription->price;
         $user->balance->save();
 
-        // Добавьте пользователя к курсу
+        // Добавьте пользователя к курсу.
         $user->courses()->attach($course);
 
         return response()->json('success', 'Course purchased successfully');
