705e3d35603ca24903f0fd0b34006b103ca98789
nusratzoda <nusratzodasuhaib@gmail.com>
2023-09-26 11:49:21 -1200

    pay

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -68,7 +68,7 @@ class AuthController extends Controller
 
     function verifyPhoneNumber(Request $request)
     {
-        dd($request);
+        // dd($request);
         $user = Auth::guard()->user();
         return $user->verifyCode($request->input('verification')) === true
             ? response()->json(['message' => 'Verification Completed'], 200)
