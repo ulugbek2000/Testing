984a17e7194a656ddb0434c520b9bd22e35f563a
nusratzoda <nusratzodasuhaib@gmail.com>
2023-09-28 10:52:12 -1200

    Course

--- routes/api.php
+++ routes/api.php
@@ -69,7 +69,7 @@ Route::post('login', [AuthController::class, 'login']);
 
 Route::middleware(['auth:sanctum'])->group(function () {
 
-    Route::middleware('access:' . implode(',',[ UserType::Admin]))->group(function () {
+    Route::middleware('access:' . [ UserType::Admin])->group(function () {
         //  Route::middleware('access:' . implode(',', [UserType::Student]))->group(function () {
 
         // //Данный админ
