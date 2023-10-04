bc5aea5b3d9d20205f25889c830272964cfbd181
nusratzoda <nusratzodasuhaib@gmail.com>
2023-10-03 18:51:58 -1200

    Token

--- app/Models/User.php
+++ app/Models/User.php
@@ -76,10 +76,10 @@ class User extends Authenticatable implements JWTSubject
     public function getJWTCustomClaims()
     {
         return [
-            'user_type' => $this->roles()->first()->id,
-            'is_phone_verified' => $this->phoneVerified(),
-            'email'=>$this->phone ?? $this->email,
-            'name'=>$this->name,
+            // 'user_type' => $this->roles()->first()->id,
+            // 'is_phone_verified' => $this->phoneVerified(),
+            // 'email'=>$this->phone ?? $this->email,
+            // 'name'=>$this->name,
         ];
     }
 
