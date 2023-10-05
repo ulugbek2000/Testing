273963de4f5140d79d33cd230b6d316bc0573155
nusratzoda <nusratzodasuhaib@gmail.com>
2023-10-05 10:17:09 -1200

    verifyCode

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -102,11 +102,11 @@ class AuthController extends Controller
     function verifyCode(Request $request)
     {
         $user = Auth::user();
-        
+        dd($request);
         return $user->verifyCode($request->input('verification')) === true
-       
+      
             ? response()->json(['message' => 'Verification Completed'], 200)
             : response()->json(['message' => 'Verification Failed'], 406);
-            dd($request);
+           
     }
 }
