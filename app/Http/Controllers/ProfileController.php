5e48724d8646981b79e09fda32401edbb2ba8ef8
nusratzoda <nusratzodasuhaib@gmail.com>
2023-10-10 16:49:52 -1200

    Massive

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -71,12 +71,13 @@ class ProfileController extends Controller
         // $user->update($data);
         $photoPath = $user->photo;
 
+        if (is_string($photoPath) && Storage::exists($photoPath)) {
+            // Удалить старую фотографию
+            Storage::delete($photoPath);
+        }
+
         if ($request->hasFile('photo')) {
 
-            // if (is_string($photoPath) && Storage::exists($photoPath)) {
-            //     // Удалить старую фотографию
-            //     Storage::delete($photoPath);
-            // }
             // Убедитесь, что файл был загружен
             $uploadedPhoto = $request->file('photo');
 
