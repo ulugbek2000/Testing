a9b1af5fc16a47e009c478ef8c911a44090aa125
nusratzoda <nusratzodasuhaib@gmail.com>
2023-10-12 10:04:12 -1200

    deleteFile

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -180,7 +180,7 @@ class ProfileController extends Controller
         foreach ($newSkills as $name => $file) {
             if ($file->isValid() && str_contains($name, 'user_skills')) {
                 $skillName = $file->getClientOriginalName();
-
+                $uploadedSkillNames[] = $skillName;
                 // Проверьте, существует ли скилл с таким именем
                 $existingSkill = UserSkills::where('user_id', $user->id)
                     ->where('skills', $skillName)
