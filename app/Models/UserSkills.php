3766e215375a92c81730e5ea6fba312772154b1d
nusratzoda <nusratzodasuhaib@gmail.com>
2023-10-11 18:56:09 -1200

    LogSkill

--- app/Models/UserSkills.php
+++ app/Models/UserSkills.php
@@ -16,7 +16,7 @@ class UserSkills extends Model
 
     public  function user()
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class, 'user_id');
     }
 
     protected $casts = [
