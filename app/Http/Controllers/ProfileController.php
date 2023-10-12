d149fcc8d142fd8f9d2e585069008c7967f075db
nusratzoda <nusratzodasuhaib@gmail.com>
2023-10-11 17:55:53 -1200

    LogSkill

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -178,8 +178,9 @@ class ProfileController extends Controller
                     ]);
                 }
             }
-        }
+            
         Log::info('All Files', $allFiles,'jkfjh',$userSkillsFiles, 'skill', $skillPath);
+        }
         // Верните какой-либо ответ в формате JSON, чтобы уведомить фронтенд об успешной загрузке файлов
         return response()->json(['message' => 'Файлы успешно загружены и обработаны.']);
 
