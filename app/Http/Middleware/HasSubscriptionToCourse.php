2755c1c65573016e45929912c7fc1b1534ef4adc
nusratzoda <nusratzodasuhaib@gmail.com>
2023-10-31 11:28:33 -1200

    lessonChange

--- app/Http/Middleware/HasSubscriptionToCourse.php
+++ app/Http/Middleware/HasSubscriptionToCourse.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
 use Symfony\Component\HttpFoundation\Response;
 
 class HasSubscriptionToCourse
@@ -16,7 +17,7 @@ class HasSubscriptionToCourse
      */
     public function handle(Request $request, Closure $next): Response
     {
-        dd($request->route()->getName());
+        dd(Route::currentRouteName());
         if( Auth::check() && Auth::user()->isSubscribed($request->get('course')) )
             return $next($request);
         else abort(403);
