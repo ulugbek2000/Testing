2c9ee10abc51a49ea34d47cdf55b20698c662397
nusratzoda <nusratzodasuhaib@gmail.com>
2023-10-31 18:32:13 -1200

    lessonChange

--- app/Http/Controllers/UserLessonProgressController.php
+++ app/Http/Controllers/UserLessonProgressController.php
@@ -20,8 +20,7 @@ class UserLessonProgressController extends Controller
     function getProgress(Course $course) {
         $user = Auth::user();
         $completedLessons = UserLessonsProgress::where('user_id', $user->id)->where('course_id', $course->id)->where('completed', true)->count();
-        $totalLessons = $course->topics()->withCount('lessons')->get()
-            ->map(function($q){return $q;});
+        $totalLessons = $course->topics()->withCount('lessons')->get();
 
         dd($totalLessons);
         $progressPercentage = ($completedLessons / $totalLessons) * 100;
