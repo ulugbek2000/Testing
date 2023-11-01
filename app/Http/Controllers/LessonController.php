9a28111bdfbc1d1dbcb206a74ea85e1140081658
nusratzoda <nusratzodasuhaib@gmail.com>
2023-10-31 16:49:37 -1200

    lessonChange

--- app/Http/Controllers/LessonController.php
+++ app/Http/Controllers/LessonController.php
@@ -79,11 +79,24 @@ class LessonController extends Controller
      */
     public function show(Lesson $lesson)
     {
-        // Return Json Response
-        return response()->json([
-            'lessons' => $lesson
-        ], 200);
+        // Выберите только нужные поля
+        $response = [
+            'id' => $lesson->id,
+            'topic_id' => $lesson->topic_id,
+            'name' => $lesson->name,
+            'duration' => $lesson->duration,
+            'cover' => $lesson->cover,
+            'content'=>$lesson->content,
+            'type' => $lesson->type,
+            'created_at' => $lesson->created_at,
+            'updated_at' => $lesson->updated_at,
+            'deleted_at' => $lesson->deleted_at,
+        ];
+    
+        // Верните JSON Response с выбранными полями
+        return response()->json($response, 200);
     }
+    
     /**
      * Show the form for editing the specified resource.
      */
