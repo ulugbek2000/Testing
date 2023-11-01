94cad2d4317d9c7726e6a252c851fcd2b9a7ab07
nusratzoda <nusratzodasuhaib@gmail.com>
2023-10-31 19:39:58 -1200

    lessonChange

--- app/Http/Controllers/LessonController.php
+++ app/Http/Controllers/LessonController.php
@@ -41,7 +41,7 @@ class LessonController extends Controller
             ];
         })->toArray());
         if($data === null){
-            return [''];
+            return response()->json([]);
         }
         
         return response()->json($data);
