274c8dff0fc797324770320daf71c51dacd084c7
nusratzoda <nusratzodasuhaib@gmail.com>
2023-11-08 11:44:11 -1200

    SubscriptionUser

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -251,7 +251,7 @@ class ProfileController extends Controller
                 $query->select('id', 'name', 'slug', 'quantity_lessons', 'hours_lessons', 'short_description', 'video', 'has_certificate', 'logo');
             }
         ]) 
-            ->select('id', 'user_id', 'subscription_id', 'course_id', 'created_at')
+            ->select('id', 'user_id', 'subscription_id', 'course_id')
             ->get();
 
         $filteredSubscriptions = $subscriptions->map(function ($subscription) {
