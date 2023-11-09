24e34c23d36abaa73f54723120bdcd189ab76822
nusratzoda <nusratzodasuhaib@gmail.com>
2023-11-09 08:58:30 -1200

    RoleUser

--- app/Http/Controllers/UserRoleController.php
+++ app/Http/Controllers/UserRoleController.php
@@ -13,13 +13,19 @@ class UserRoleController extends Controller
     public function getAllUsers(Request $request)
     {
         $user = Auth::user();
-
+    
         if ($user->hasRole(UserType::Admin)) {
             $per_page = $request->per_page ?? 12;
-
-            $users = User::with('roles')->paginate($per_page);
-
+    
+            $users = User::paginate($per_page);
+    
+            foreach ($users as $user) {
+                $role = $user->roles()->first(); // Получение первой роли пользователя
+                $user->role = $role->name; // Предположим, что имя роли находится в поле "name"
+            }
+    
             return UserResource::collection($users);
         }
     }
+    
 }
