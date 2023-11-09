ddd4b1555c5f9286ed93e0e45393c3d7f9cb13e6
nusratzoda <nusratzodasuhaib@gmail.com>
2023-11-09 10:13:38 -1200

    RoleUser

--- config/auth.php
+++ config/auth.php
@@ -67,7 +67,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => App\Enums\UserType::getValues(),
+            'model' => App\Models\User::class,
         ],
 
         // 'users' => [
