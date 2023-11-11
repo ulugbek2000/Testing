be3e428e0f374caa973e4690d03dc0936484c8b3
nusratzoda <nusratzodasuhaib@gmail.com>
2023-11-10 17:51:56 -1200

    Check1

--- routes/api.php
+++ routes/api.php
@@ -75,6 +75,7 @@ Route::get('course/{course}/topics', [TopicController::class, 'index']);
 Route::get('topic/{topic}/lessons', [LessonController::class, 'index']);
 Route::get('lesson/{lesson}', [LessonController::class, 'show']);
 
+Route::get('getAllCourse/enrolled/users', [ProfileController::class, 'getCourseWithEnroledUsers']);
 
 Route::middleware(['jwt.auth'])->group(function () {
 
@@ -87,7 +88,7 @@ Route::middleware(['jwt.auth'])->group(function () {
         Route::get('admin/account', [ProfileController::class, 'getProfile']);
 
         // Получение курсы с свзи пользователи 
-        Route::get('getAllCourse/enrolled/users', [ProfileController::class, 'getCourseWithEnroledUsers']);
+      
 
         //Получение все пользователи
         Route::get('getAllUsers', [UserRoleController::class, 'getAllUsers']);
