69fd4fa76c79d65d58958fb983e4f042dead2652
nusratzoda <nusratzodasuhaib@gmail.com>
2023-11-13 09:15:56 -1200

    check

--- routes/api.php
+++ routes/api.php
@@ -81,7 +81,7 @@ Route::get('lesson/{lesson}', [LessonController::class, 'show']);
 
 Route::middleware(['jwt.auth'])->group(function () {
 
-    Route::middleware(['access' . UserType::Admin])->group(function () {
+    Route::middleware(['access:' . UserType::Admin])->group(function () {
 
         //Пополнение баланс:
         Route::post('admin/balance/deposit', [UserTransactionController::class, 'topUpWallet']);
