65c697d601a1215ce694c5b56bbb877928a9b679
nusratzoda <nusratzodasuhaib@gmail.com>
2023-11-14 10:42:48 -1200

    StatisticsUser

--- app/Http/Controllers/StatisticsController.php
+++ app/Http/Controllers/StatisticsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Enums\UserType;
+use App\Models\Course;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -10,15 +11,10 @@ class StatisticsController extends Controller
 {
     public function getStatisticsUser()
     {
-        $users = User::with('courses')->get();
+        // $users = User::with('courses')->get();
 
-        $courseCount = 0;
+        $courseCount = Course::all()->count();
 
-        foreach ($users as $user) {
-            foreach ($user->courses as $course) {
-                $courseCount++;
-            }
-        }
         $studentsCount = User::whereHas('roles', function ($query) {
             $query->where('name', UserType::Student);
         })->count();
