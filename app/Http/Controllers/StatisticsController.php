32eadc22d3e2fddcb15bd4794d4e8465636bff0a
nusratzoda <nusratzodasuhaib@gmail.com>
2023-11-14 10:38:52 -1200

    StatisticsUser

--- app/Http/Controllers/StatisticsController.php
+++ app/Http/Controllers/StatisticsController.php
@@ -10,17 +10,27 @@ class StatisticsController extends Controller
 {
     public function getStatisticsUser()
     {
+        $users = User::with('courses')->get();
+
+        $courseCount = 0;
+
+        foreach ($users as $user) {
+            foreach ($user->courses as $course) {
+                $courseCount++;
+            }
+        }
         $studentsCount = User::whereHas('roles', function ($query) {
             $query->where('name', UserType::Student);
         })->count();
-    
+
         $teachersCount = User::whereHas('roles', function ($query) {
             $query->where('name', UserType::Teacher);
         })->count();
-    
+
         return response()->json([
             'students_count' => $studentsCount,
             'teachers_count' => $teachersCount,
+            'course_count' => $courseCount,
         ]);
     }
 }
