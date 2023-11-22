0e0a73ad90ca653dae0bf29276daec8e8654c6fe
nusratzoda <nusratzodasuhaib@gmail.com>
2023-11-22 10:28:46 -1200

    forgotPassword

--- database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
+++ database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
@@ -12,9 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary()->default(null);
+            // $table->string('email')->primary();
             $table->string('token');
-            $table->string('phone')->default(null);
+            $table->string('phone');
             $table->timestamp('created_at')->nullable();
         });
     }
