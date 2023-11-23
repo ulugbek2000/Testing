7d351026efe6ceb89d8e7b788bc089b6873bbb9b
nusratzoda <nusratzodasuhaib@gmail.com>
2023-11-22 16:38:23 -1200

    addPhoneColumn

--- app/Http/Controllers/PasswordResetTokenController.php
+++ app/Http/Controllers/PasswordResetTokenController.php
@@ -4,30 +4,41 @@ namespace App\Http\Controllers;
 
 use App\Models\PasswordResetToken;
 use App\Models\User;
+use App\Notifications\VerificationNotification;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Password;
 
 class PasswordResetTokenController extends Controller
 {
+    use App\Notifications\VerificationCodeNotification;
+
     public function sendResetLink(Request $request)
     {
         $request->validate(['phone' => 'required|string']);
-
+    
         $user = User::where('phone', $request->phone)->first();
-
+    
         if (!$user) {
             return response()->json(['error' => 'User not found'], 404);
         }
-
-        $credentials = ['phone' => $user->phone];
-        try {
-            
-            Password::rand(1000,9999);
     
-            return response()->json(['message' => 'Password reset link sent'], 200);
+        $verificationCode = rand(1000, 9999);
+    
+        try {
+            $user->notify(new VerificationNotification($verificationCode));
+    
+            PasswordResetToken::create([
+                'email' => $user->email,
+                'phone' => $user->phone,
+                'token' => Hash::make($verificationCode),
+                'verification_code' => $verificationCode,
+            ]);
+    
+            return response()->json(['message' => 'Verification code sent'], 200);
         } catch (\Exception $e) {
             return response()->json(['error' => $e->getMessage()], 500);
         }
     }
+    
 }
--- app/Listeners/NewUserListener.php
+++ app/Listeners/NewUserListener.php
@@ -27,8 +27,10 @@ class NewUserListener
         $event->user->assignRole(UserType::Student);
         // TODO create user wallet upon register
         // $event->user->wallet()->create(['wallet' => 0]);
+
         // Send sms verification notification
         $verificationNumber = rand(1000, 9999);
         $event->user->notify(new VerificationNotification($verificationNumber));
+
     }
 }
