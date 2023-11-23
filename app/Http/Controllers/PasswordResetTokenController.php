105c65c2c5084c76a2de4510bbdb28dce032416a
nusratzoda <nusratzodasuhaib@gmail.com>
2023-11-23 11:56:33 -1200

    resetPassword

--- app/Http/Controllers/PasswordResetTokenController.php
+++ app/Http/Controllers/PasswordResetTokenController.php
@@ -18,20 +18,21 @@ class PasswordResetTokenController extends Controller
     public function sendCodeReset(Request $request)
     {
         $request->validate(['phone' => 'required|string']);
-    
+
         $user = User::where('phone', $request->phone)->first();
-    
-       
-   
-    
+
+
+
+
         $verificationCode = rand(1000, 9999);
-    
-        if ($user->phone == $request->phone) {
-            $user->notify(new VerificationNotification($verificationCode));
-    
-            return response()->json(['message' => 'Verification code sent'], 200);
+
+        if ($user->phone != $request->phone) {
+            return response()->json('Номер телефон не авторизован');
         }
-        return response()->json('Номер телефон не авторизован');
+
+        $user->notify(new VerificationNotification($verificationCode));
+
+        return response()->json(['message' => 'Verification code sent'], 200);
     }
 
     public function resetPassword(Request $request)
@@ -40,16 +41,16 @@ class PasswordResetTokenController extends Controller
             'verification' => 'required|numeric',
             'password' => ['required', 'string', 'min:8', 'regex:/^(?=.*[0-9])(?=.*[a-zA-Z]).*$/', 'confirmed'],
         ]);
-    
+
         $user = Auth::user();
-    
+
         $verificationCode = $request->input('verification');
-    
+
         if ($user->verifyCode($verificationCode)) {
             $user->update(['password' => bcrypt($request->password)]);
             return response()->json(['message' => 'Пароль успешно изменен'], 200);
         }
-    
+
         return response()->json(['error' => 'Неверный код подтверждения'], 422);
     }
 }
