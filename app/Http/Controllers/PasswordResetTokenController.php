f8d2348642d80ba57a588c7d186456d55d08131a
nusratzoda <nusratzodasuhaib@gmail.com>
2023-11-24 10:06:09 -1200

    resetPassword

--- app/Http/Controllers/PasswordResetTokenController.php
+++ app/Http/Controllers/PasswordResetTokenController.php
@@ -38,24 +38,19 @@ class PasswordResetTokenController extends Controller
             'verification' => 'required|numeric',
             'password' => ['required', 'string', 'min:8', 'regex:/^(?=.*[0-9])(?=.*[a-zA-Z]).*$/', 'confirmed'],
         ]);
-    
+
         $notification = Notification::where('data', $request->verification)->first();
-    
+
         if ($notification && $notification->notifiable_type === 'App\Models\User') {
-           
+
             $user = User::find($notification->notifiable_id);
-    
+
             if ($user) {
                 $user->update(['password' => bcrypt($request->password)]);
                 return response()->json(['message' => 'Пароль успешно изменен'], 200);
-            } else {
-                info('Пользователь не найден');
             }
-        } else {
-            info('Уведомление не найдено или notifiable не типа User');
+
+            return response()->json(['error' => 'Неверный код подтверждения'], 422);
         }
-    
-        return response()->json(['error' => 'Неверный код подтверждения'], 422);
     }
-    
 }
