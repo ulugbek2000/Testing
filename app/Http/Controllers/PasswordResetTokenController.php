84afb61b21d60f966a7b20f4f2e799b31a622190
nusratzoda <nusratzodasuhaib@gmail.com>
2023-11-24 10:16:10 -1200

    resetPassword

--- app/Http/Controllers/PasswordResetTokenController.php
+++ app/Http/Controllers/PasswordResetTokenController.php
@@ -2,19 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Notification;
 use App\Models\PasswordResetToken;
 use App\Models\User;
 use App\Notifications\VerificationNotification;
 use Illuminate\Http\Request;
-use Illuminate\Notifications\Notification as NotificationsNotification;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Notification as FacadesNotification;
 use Illuminate\Support\Facades\Password;
 use Tymon\JWTAuth\Facades\JWTAuth;
-use Vonage\Voice\Webhook\Notification as WebhookNotification;
-
+use Illuminate\Support\Facades\Notification;
 class PasswordResetTokenController extends Controller
 {
 
@@ -35,25 +31,31 @@ class PasswordResetTokenController extends Controller
         return response()->json(['message' => 'Код подтверждения отправлен'], 200);
     }
 
+
+    
     public function resetPassword(Request $request)
     {
         $request->validate([
             'verification' => 'required|numeric',
             'password' => ['required', 'string', 'min:8', 'regex:/^(?=.*[0-9])(?=.*[a-zA-Z]).*$/', 'confirmed'],
         ]);
-
-        $notification = FacadesNotification::where('data', $request->verification)->first();
-
+    
+        // Retrieve the notification using the verification code
+        $notification = Notification::where('data->verification_code', $request->verification)->first();
+    
         if ($notification && $notification->notifiable_type === 'App\Models\User') {
-
             $user = User::find($notification->notifiable_id);
-
+    
             if ($user) {
+                // Update the user's password
                 $user->update(['password' => bcrypt($request->password)]);
                 return response()->json(['message' => 'Пароль успешно изменен'], 200);
             }
-
+    
             return response()->json(['error' => 'Неверный код подтверждения'], 422);
         }
+    
+        return response()->json(['error' => 'Неверный код подтверждения'], 422);
     }
+    
 }
