f60b89b747f7149aa757306fc53e4249bade4898
nusratzoda <nusratzodasuhaib@gmail.com>
2023-11-29 10:13:19 -1200

    AddDuration

--- app/Models/Lesson.php
+++ app/Models/Lesson.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
-
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
 class Lesson extends Model implements HasMedia
 {
@@ -30,5 +30,12 @@ class Lesson extends Model implements HasMedia
 
     function getContentMedia() {
         return $this->getFirstMedia("content");
+    }
+    public function registerMediaConversions(Media $media = null): void
+    {
+        $this->addMediaConversion('thumb')
+            ->width(100)
+            ->height(100);
+        
     }
 }
