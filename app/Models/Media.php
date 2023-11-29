724e6c5e74472a97077e9989656e75449ece07d6
nusratzoda <nusratzodasuhaib@gmail.com>
2023-11-29 11:03:08 -1200

    AddDuration

--- app/Models/Media.php
+++ app/Models/Media.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use FFMpeg\FFMpeg;
 use FFMpeg\FFProbe;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -19,17 +20,29 @@ class Media extends BaseMedia
         static::created(static function (Media $media) {
 
             if ($media->type === 'video' or $media->type === 'audio') {
+                // Предполагается, что у вас есть объект $media, представляющий ваш медиа-файл.
 
-			      $ffmpeg = FFProbe::create();
-			      
-              	  $duration = $ffmpeg->format($media->getPath())->get('duration');
+                $ffmpeg = FFMpeg::create();
 
-                  $media
-                    ->setCustomProperty('duration', $duration)
-                    ->save();
-                    
+                // Получаем длительность файла
+                $duration = $ffmpeg->open($media->getPath())->getDurationInSeconds();
+
+                // Сохраняем длительность в пользовательских свойствах медиа
+                $media->setCustomProperty('duration', $duration)->save();
+
+
+                
+                // if ($media->type === 'video' or $media->type === 'audio') {
+
+                //       $ffmpeg = FFProbe::create();
+
+                //   	  $duration = $ffmpeg->format($media->getPath())->get('duration');
+
+                //       $media
+                //         ->setCustomProperty('duration', $duration)
+                //         ->save();
+
             }
-
         });
     }
 }
