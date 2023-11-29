c993992f73a0c5b40f0dd3dcaebaeadb5ea6acd9
nusratzoda <nusratzodasuhaib@gmail.com>
2023-11-29 10:25:56 -1200

    AddDuration

--- app/Http/Controllers/LessonController.php
+++ app/Http/Controllers/LessonController.php
@@ -69,6 +69,7 @@ class LessonController extends Controller
         if ($request->input('type') !== 'text') {
             $media = $lesson->addMedia($request->file('content'))->toMediaCollection('content');
             $media->model_type = Lesson::class;
+            $media->model_id = $lesson->id;
             $media->save();
             $lesson->content = $media->getPath();
             $lesson->duration = round($media->getCustomProperty('duration') / 60);
