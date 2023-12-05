c5ef5fa6ca3bbc33165127c4c73b4da330dee087
nusratzoda <nusratzodasuhaib@gmail.com>
2023-12-05 10:03:37 -1200

    CreateFFProbe

--- app/Models/Media.php
+++ app/Models/Media.php
@@ -15,42 +15,34 @@ use Spatie\MediaLibrary\MediaCollections\Models\Media as BaseMedia;
 class Media extends BaseMedia implements HasMedia
 {
     use HasFactory, InteractsWithMedia;
-
-    class Media extends BaseMedia implements HasMedia
+    protected static function boot()
     {
-        use HasFactory, InteractsWithMedia;
-    
-        protected static function boot()
-        {
-            parent::boot();
-    
-            static::saving(static function (Media $media) {
-                if ($media->type === 'video' || $media->type === 'audio') {
-                    $ffmpeg = FFMpeg::create([
-                        'ffmpeg.binaries' => '/home/softclub/domains/lmsapi.softclub.tj/ffmpeg-git-20231128-amd64-static/ffmpeg',
-                        'ffprobe.binaries' => '/home/softclub/domains/lmsapi.softclub.tj/ffmpeg-git-20231128-amd64-static/ffprobe'
-                    ]);
-    
-                    $uploadedFile = $media->file;
-    
-                    if ($uploadedFile) {
-                        // Добавляем файл к коллекции 'content'
-                        $media->addMedia($uploadedFile)->toMediaCollection('content');
-    
-                        // После добавления файла в коллекцию, вы можете получить путь к нему
-                        $localPath = $media->getPath();
-    
-                        $video = $ffmpeg->open($localPath);
-    
-                        $duration = $ffmpeg->getFFProbe()->format($video)->get('duration');
-    
-                        $media
-                            ->setCustomProperty('duration', $duration);
-                    }
+        parent::boot();
+
+        static::saving(static function (Media $media) {
+            if ($media->type === 'video' || $media->type === 'audio') {
+                $ffmpeg = FFMpeg::create([
+                    'ffmpeg.binaries' => '/home/softclub/domains/lmsapi.softclub.tj/ffmpeg-git-20231128-amd64-static/ffmpeg',
+                    'ffprobe.binaries' => '/home/softclub/domains/lmsapi.softclub.tj/ffmpeg-git-20231128-amd64-static/ffprobe'
+                ]);
+
+                $uploadedFile = $media->file;
+
+                if ($uploadedFile) {
+                    // Добавляем файл к коллекции 'content'
+                    $media->addMedia($uploadedFile)->toMediaCollection('content');
+
+                    // После добавления файла в коллекцию, вы можете получить путь к нему
+                    $localPath = $media->getPath();
+
+                    $video = $ffmpeg->open($localPath);
+
+                    $duration = $ffmpeg->getFFProbe()->format($video)->get('duration');
+
+                    $media
+                        ->setCustomProperty('duration', $duration);
                 }
-            });
-        }
+            }
+        });
     }
-}    
-
-
+}`
