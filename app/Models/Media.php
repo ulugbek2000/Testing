f75865336e1ee994c95016e5b45af9a7128449d8
nusratzoda <nusratzodasuhaib@gmail.com>
2023-12-05 09:54:43 -1200

    CreateFFProbe

--- app/Models/Media.php
+++ app/Models/Media.php
@@ -7,12 +7,14 @@ use FFMpeg\FFProbe;
 use getID3;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 use Spatie\MediaLibrary\MediaCollections\Models\Media as BaseMedia;
 
 
-class Media extends BaseMedia
+class Media extends BaseMedia implements HasMedia
 {
-    use HasFactory;
+    use HasFactory, InteractsWithMedia;
 
     protected static function boot()
     {
