5a934a32fd44f029f1636a763d02bf47d73890a4
nusratzoda <nusratzodasuhaib@gmail.com>
2023-12-05 09:50:21 -1200

    CreateFFProbe

--- app/Models/Media.php
+++ app/Models/Media.php
@@ -26,7 +26,7 @@ class Media extends BaseMedia
                 ]);
 
                 // Получаем объект файла из запроса
-                $uploadedFile = $media->getFirstMedia();
+                $uploadedFile = $media->content;
 
                 // Сохраняем файл локально
                 $localPath = $uploadedFile->store('content');
