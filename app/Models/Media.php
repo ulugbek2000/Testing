cbf17142fce09f7a0e85d4b8fedfaecff9c49307
nusratzoda <nusratzodasuhaib@gmail.com>
2023-12-05 12:08:21 -1200

    FFProbeDuration

--- app/Models/Media.php
+++ app/Models/Media.php
@@ -21,26 +21,24 @@ class Media extends BaseMedia implements HasMedia
     {
         parent::boot();
 
-       function (Media $media) {
+        function (Media $media) {
             if ($media->type === 'video' || $media->type === 'audio') {
-                $ffmpeg = FFMpeg::create([
-                    'ffmpeg.binaries' => '/home/softclub/domains/lmsapi.softclub.tj/ffmpeg-git-20231128-amd64-static/ffmpeg',
-                    'ffprobe.binaries' => '/home/softclub/domains/lmsapi.softclub.tj/ffmpeg-git-20231128-amd64-static/ffprobe'
-                ]);
+                $ffmpegPath = '/home/softclub/domains/lmsapi.softclub.tj/ffmpeg-git-20231128-amd64-static/ffmpeg';
+                $ffprobePath =  '/home/softclub/domains/lmsapi.softclub.tj/ffmpeg-git-20231128-amd64-static/ffprobe';
+
+
+                FFMpeg::setFFMpegBinary($ffmpegPath);
+                FFMpeg::setFFProbeBinary($ffprobePath);
 
                 $uploadedFile = $media->file;
 
                 if ($uploadedFile) {
                     $media->addMedia($uploadedFile)->toMediaCollection('content');
-                    $localPath = $media->getPath();
+                    // $localPath = $media->getPath();
 
-                    $video = $ffmpeg->open($localPath);
+                    // $video = $ffmpegPath->open($localPath);
 
-                    // Получаем длительность через FFprobe
                     $duration = $video->getDurationInSeconds();
-                    // Log::info('Streams: ' . print_r($video->getStreams(), true));
-                    // Log::info('Duration: ' . $duration);
-
                     if ($duration !== null) {
                         $media->setCustomProperty('duration', $duration);
                     }
