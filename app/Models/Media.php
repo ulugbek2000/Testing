a844ed1e1bc99cc47f2f61b40bfc70eb3446732c
nusratzoda <nusratzodasuhaib@gmail.com>
2023-12-06 09:39:49 -1200

    FormatFFMpeg

--- app/Models/Media.php
+++ app/Models/Media.php
@@ -23,7 +23,7 @@ class Media extends BaseMedia implements HasMedia
 
         static::saving(function (Media $media) {
             if ($media->type === 'video' || $media->type === 'audio') {
-                $ffmpeg = FFMpeg::create([
+                $ffmpeg = FFProbe::create([
                     'ffmpeg.binaries' => '/home/softclub/domains/lmsapi.softclub.tj/ffmpeg-git-20231128-amd64-static/ffmpeg',
                     'ffprobe.binaries' => '/home/softclub/domains/lmsapi.softclub.tj/ffmpeg-git-20231128-amd64-static/ffprobe'
                 ]);
