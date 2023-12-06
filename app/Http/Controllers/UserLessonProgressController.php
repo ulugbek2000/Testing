418c7fcfd8361cbf12fcd2025f5b474b8e6d2185
nusratzoda <nusratzodasuhaib@gmail.com>
2023-12-06 10:56:58 -1200

    FormatFFMpeg

--- app/Http/Controllers/UserLessonProgressController.php
+++ app/Http/Controllers/UserLessonProgressController.php
@@ -37,7 +37,11 @@ class UserLessonProgressController extends Controller
     public function showActivity()
     {
         $user = Auth::user();
-        $watchedLessons = $user->courses()->topics->lessons;
+        $watchedLessons = $user->courses->flatMap(function ($course) {
+            return $course->topics->flatMap(function ($topic) {
+                return $topic->lessons;
+            });
+        });
 
         // Определить текущую неделю и начало текущего дня
         $currentWeekStart = Carbon::now()->startOfWeek();
