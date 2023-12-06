6c415149487ab700f86083dd4ef2fe18b6b3b6ee
nusratzoda <nusratzodasuhaib@gmail.com>
2023-12-06 11:29:17 -1200

    FormatFFMpeg

--- app/Http/Controllers/UserLessonProgressController.php
+++ app/Http/Controllers/UserLessonProgressController.php
@@ -39,43 +39,52 @@ class UserLessonProgressController extends Controller
         $userProgress = UserLessonsProgress::where('user_id', $user->id)->get();
 
         $currentWeekStart = Carbon::now()->startOfWeek();
-        $results = [];
+       //...
 
-        for ($i = Carbon::MONDAY; $i <= Carbon::SUNDAY; $i++) {
-            $dayStart = $currentWeekStart->copy()->day($i);
+$results = [];
 
-            // Выводим информацию об итерации для отладки
-            echo "Processing day: {$dayStart->format('l')} ({$dayStart->format('Y-m-d')})\n";
+for ($i = Carbon::MONDAY; $i <= Carbon::SUNDAY; $i++) {
+    $dayStart = $currentWeekStart->copy()->day($i);
 
-            // Фильтруем по дате прогресса
-            $watchedInDay = $userProgress->filter(function ($progress) use ($dayStart) {
-                return Carbon::parse($progress->created_at)->isSameDay($dayStart);
-            });
+    // Выводим информацию об итерации для отладки
+    echo "Processing day: {$dayStart->format('l')} ({$dayStart->format('Y-m-d')})\n";
 
-            // Получаем lesson_id для просмотренных уроков в этот день
-            $lessonIds = $watchedInDay->pluck('lesson_id')->toArray();
+    // Фильтруем по дате прогресса
+    $watchedInDay = $userProgress->filter(function ($progress) use ($dayStart) {
+        return Carbon::parse($progress->created_at)->isSameDay($dayStart);
+    });
 
-            // Выводим lessonIds для отладки
-            echo "Lesson IDs for the day: " . implode(', ', $lessonIds) . "\n";
+    // Выводим информацию о прогрессе за день для отладки
+    echo "Progress for the day: ";
+    print_r($watchedInDay->toArray());
 
-            // Получаем общую продолжительность просмотренных уроков в этот день
-            $totalMinutesWatched = Lesson::whereIn('id', $lessonIds)->sum('duration');
+    // Получаем lesson_id для просмотренных уроков в этот день
+    $lessonIds = $watchedInDay->pluck('lesson_id')->toArray();
 
-            // Выводим totalMinutesWatched для отладки
-            echo "Total minutes watched for the day: $totalMinutesWatched\n";
+    // Выводим lessonIds для отладки
+    echo "Lesson IDs for the day: " . implode(', ', $lessonIds) . "\n";
 
-            // Добавляем общую продолжительность в результаты
-            $results[$dayStart->format('l')] = $totalMinutesWatched;
+    // Получаем общую продолжительность просмотренных уроков в этот день
+    $totalMinutesWatched = Lesson::whereIn('id', $lessonIds)->sum('duration');
 
-            echo "Processed day: {$dayStart->format('l')} ({$dayStart->format('Y-m-d')})\n\n";
-        }
+    // Выводим totalMinutesWatched для отладки
+    echo "Total minutes watched for the day: $totalMinutesWatched\n";
 
-        // Добавим дополнительный вывод для отладки
-        dd([
-            'current_week_start' => $currentWeekStart,
-            'user_progress' => $userProgress,
-            'results' => $results,
-        ]);
+    // Добавляем общую продолжительность в результаты
+    $results[$dayStart->format('l')] = $totalMinutesWatched;
+
+    echo "Processed day: {$dayStart->format('l')} ({$dayStart->format('Y-m-d')})\n\n";
+}
+
+// Добавим дополнительный вывод для отладки
+dd([
+    'current_week_start' => $currentWeekStart,
+    'user_progress' => $userProgress,
+    'results' => $results,
+]);
+
+//...
+
 
         return response()->json($results);
     }
