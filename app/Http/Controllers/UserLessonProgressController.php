2baa98b026a9f24516c863a4008453ba6a89aca9
nusratzoda <nusratzodasuhaib@gmail.com>
2023-12-07 10:10:34 -1200

    DayShow

--- app/Http/Controllers/UserLessonProgressController.php
+++ app/Http/Controllers/UserLessonProgressController.php
@@ -36,19 +36,22 @@ class UserLessonProgressController extends Controller
 
     public function showActivity(Request $request)
     {
-        $phone = $request->input('phone'); // Предположим, что вы передаете email в запросе
-
+        $phone = $request->input('phone');
+    
         $user = User::where('phone', $phone)->first();
+    
+        if (!$user) {
+            // Обработка, если пользователь не найден
+            return response()->json(['error' => 'User not found'], 404);
+        }
+    
         $userProgress = UserLessonsProgress::where('user_id', $user->id)->get();
-
+    
         $currentWeekStart = Carbon::now()->startOfWeek();
-
         $results = [];
-
-      
+    
         $weekStartDate = $currentWeekStart->format('d.m.Y');
         $weekEndDate = $currentWeekStart->copy()->endOfWeek()->format('d.m.Y');
-
     
         foreach (Carbon::getDays() as $day) {
             $results[] = [
@@ -56,27 +59,28 @@ class UserLessonProgressController extends Controller
                 'total_minutes_watched' => 0,
             ];
         }
-
+    
         for ($i = Carbon::MONDAY; $i <= Carbon::SUNDAY; $i++) {
             $dayStart = $currentWeekStart->copy()->day($i);
-
+    
             $watchedInDay = $userProgress->filter(function ($progress) use ($dayStart) {
                 return Carbon::parse($progress->created_at)->isSameDay($dayStart);
             });
-
+    
             $lessonIds = $watchedInDay->pluck('lesson_id')->toArray();
-
+    
             $totalMinutesWatched = Lesson::whereIn('id', $lessonIds)->sum('duration');
-
+    
             // Обновляем результаты для каждого дня недели
             $results[$dayStart->dayOfWeek]['total_minutes_watched'] = $totalMinutesWatched;
             $results[$dayStart->dayOfWeek]['date_range'] = $dayStart->format('d.m.Y') . ' - ' . $dayStart->copy()->endOfDay()->format('d.m.Y');
         }
-
+    
         $results[] = [
             'date_range' => $weekStartDate . ' - ' . $weekEndDate,
         ];
-
+    
         return response()->json($results);
     }
+    
 }
