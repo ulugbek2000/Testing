e3910f9b294282e5590ffe61dbe4323ca8632f57
nusratzoda <nusratzodasuhaib@gmail.com>
2023-12-07 11:41:34 -1200

    DayShow

--- app/Http/Controllers/LessonController.php
+++ app/Http/Controllers/LessonController.php
@@ -74,7 +74,7 @@ class LessonController extends Controller
         if ($request->type === 'text') {
             $lesson->content = $request->input('content');
         } elseif ($request->type === 'video' || $request->type === 'audio') {
-            $lesson->addMediaFromRequest($request->input('content'))->toMediaCollection('content');
+            $media = $lesson->addMediaFromRequest('content')->toMediaCollection('content');
             // $media->model_type = Lesson::class;
             // $media->save();
             
@@ -83,10 +83,10 @@ class LessonController extends Controller
             // $durationInMinutes = round($durationInSeconds / 60);
             
             // $lesson->duration = $durationInMinutes;
-            
+            $lesson->content = $media->getUrl();   
         }
 
-        $lesson->content = in_array($request->type, [LessonTypes::Video, LessonTypes::Audio]);
+        // $lesson->content = in_array($request->type, [LessonTypes::Video, LessonTypes::Audio]);
         $lesson->save();
         return response()->json(['message' => 'Урок успешно создан.']);
     }
