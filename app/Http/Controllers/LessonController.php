01476a4da8ee3a6ed380c0c06196f55d92d1560c
nusratzoda <nusratzodasuhaib@gmail.com>
2023-12-11 14:25:25 -1200

    LessonProgress

--- app/Http/Controllers/LessonController.php
+++ app/Http/Controllers/LessonController.php
@@ -96,7 +96,7 @@ class LessonController extends Controller
              $durationInSeconds = $ffmpeg->format($localPath)->get('duration');
      
              $media->setCustomProperty('duration', $durationInSeconds)->save();
-            //  $lesson->content = $media->getUrl();
+             $lesson->content = $media->getUrl();
             //  $lesson->duration = $media->custom_properties;
          }
      
