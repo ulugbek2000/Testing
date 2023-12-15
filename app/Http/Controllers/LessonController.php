2ce7af9144f153310f730672305ebab992bb3cb1
nusratzoda <nusratzodasuhaib@gmail.com>
2023-12-15 10:38:21 +0500

    getUserType

--- app/Http/Controllers/LessonController.php
+++ app/Http/Controllers/LessonController.php
@@ -33,7 +33,7 @@ class LessonController extends Controller
     {
         $lessons = $topic->lessons;
     
-        if (Auth::check() && Auth::user()->isSubscribed($topic->course)) {    
+        if (Auth::check() && Auth::user()->isSubscribed($topic->course) or UserType::Admin) {    
             return response()->json(['data' => $lessons]);
         }
     
