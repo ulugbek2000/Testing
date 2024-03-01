78669da71742989e05acf24e9669df13e0c96433
nusratzoda <nusratzodasuhaib@gmail.com>
2024-03-01 18:09:28 +0500

    Test

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -185,15 +185,6 @@ class CourseController extends Controller
         return response()->json(['message' => $userCourse->wasRecentlyCreated ? "User enrolled to course successfuly." : "User already enrolled!"], 200);
     }
 
-
-
-
-
-
-
-
-
-    
     public function addTeachersToCourse(Request $request, Course $course)
     {
         $teacherIds = $request->input('teacher_ids', []);
