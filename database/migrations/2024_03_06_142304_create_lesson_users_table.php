65dcf81645efafdba574e4b70ac86bbee740dda3
nusratzoda <nusratzodasuhaib@gmail.com>
2024-03-07 11:40:13 +0500

    Test

--- database/migrations/2024_03_06_142304_create_lesson_users_table.php
+++ database/migrations/2024_03_06_142304_create_lesson_users_table.php
@@ -12,6 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('lesson_users', function (Blueprint $table) {
+            $table->id();
             $table->unsignedBigInteger('user_id');
             $table->unsignedBigInteger('lesson_id');
             $table->integer('views')->default(0);
