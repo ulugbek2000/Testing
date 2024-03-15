d4027f7b16adfb05bc834fffb131a181e043172e
nusratzoda <nusratzodasuhaib@gmail.com>
2024-03-15 14:12:30 +0500

    Paginate

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -14,6 +14,26 @@ class UserResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'surname' => $this->surname,
+            'phone' => $this->phone,
+            'city' => $this->city,
+            'description' => $this->description,
+            'position' => $this->position,
+            'email_verified_at' => $this->email_verified_at,
+            'phone_verified_at' => $this->phone_verified_at,
+            'date_of_birth' => $this->date_of_birth,
+            'photo' => $this->photo,
+            'two_factor_secret' => $this->two_factor_secret,
+            'two_factor_recovery_codes' => $this->two_factor_recovery_codes,
+            'two_factor_confirmed_at' => $this->two_factor_confirmed_at,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+            'phone' => $this->phone,
+            'gender' => $this->gender,
+            'deleted_at' => $this->deleted_at,
+        ];
     }
 }
