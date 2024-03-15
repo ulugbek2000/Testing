3f71addd31ded13e0072d89dfd67dd2ba405b416
nusratzoda <nusratzodasuhaib@gmail.com>
2024-03-15 14:22:08 +0500

    Paginate

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -34,6 +34,7 @@ class UserResource extends JsonResource
             'phone' => $this->phone,
             'gender' => $this->gender,
             'deleted_at' => $this->deleted_at,
+            'is_block' => $this->is_block,
         ];
     }
 }
