0cadc022c7c1972f4df17d3b3cf507dd7d42d326
nusratzoda <nusratzodasuhaib@gmail.com>
2024-04-21 15:32:06 +0500

    Firebase

--- app/Broadcasting/SmsChannel.php
+++ app/Broadcasting/SmsChannel.php
@@ -32,28 +32,11 @@ class SmsChannel
      */
     public function send($notifiable, Notification $notification)
     {
-        // $message = $notification->toSms($notifiable);
-        // // $message = $notification->toMail($notifiable);
-        // $message->send();
+        $message = $notification->toSms($notifiable);
+        // $message = $notification->toMail($notifiable);
+        $message->send();
 
 
-        if ($notification instanceof \App\Notifications\VerificationNotification) {
-            // Отправка SMS
-            $message = $notification->toSms($notifiable);
-            // Предполагается, что SmsMessage реализован соответствующим образом
-            // и возвращает экземпляр \OsonSMS\SMSGateway\SMSGateway для отправки SMS
-            $smsGateway = $message->send();
-    
-            // Отправка электронной почты
-            $message = $notification->toMail($notifiable);
-            $message->send();
-    
-            // Можете также записать логику отправки SMS-уведомления на ваш смс-шлюз здесь
-            // $smsGateway->sendSMS($message->phone, $message->content);
-        } else {
-            // Обработка других типов уведомлений (например, уведомлений по электронной почте)
-            $message = $notification->toMail($notifiable);
-            $message->send();
-        }
+        
     }
 }
