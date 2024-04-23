f54c4dba2f6ed7f064e7e8ca75eadc1a04ecfe66
nusratzoda <nusratzodasuhaib@gmail.com>
2024-04-23 15:24:37 +0500

    TestWatchLesson

--- app/Models/User.php
+++ app/Models/User.php
@@ -93,7 +93,7 @@ class User extends Authenticatable implements JWTSubject
 
     public function completedLessons()
     {
-        return $this->belongsToMany(Lesson::class, 'user_lessons_progress', 'user_id', 'lesson_id')->withTimestamps();
+        return $this->belongsToMany(Lesson::class, 'user_lessons_progress');
     }
 
     public function getJWTCustomClaims()
